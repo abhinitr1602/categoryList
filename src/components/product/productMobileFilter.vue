ba8c943f650b936c05d220ac8825c7867ea7b5e5
abhinitr1602 <abhinitr@hcl.com>
2019-12-02 18:43:22 +0530

    responsive material

--- src/components/product/productMobileFilter.vue
+++ src/components/product/productMobileFilter.vue
@@ -1,45 +1,52 @@
 <template>
-    <div class="full">
-        <div class="col-lg-12 col-md-12 ccol-sm-12 col-xs-12 brd-bottom">
-            <ul class="list-inline mobile-filter">
-                <li>
-                    <h4><b><i class="fa fa-sort"></i> Sort</b></h4>
-                </li>
-                <li >
-                    <h4><b><i class="fa fa-filter"></i> Filter</b></h4>
-                </li>
-            </ul>
-        </div>
+  <div class="full">
+    <div class="col-lg-12 col-md-12 ccol-sm-12 col-xs-12 brd-bottom">
+      <ul class="list-inline mobile-filter">
+        <li>
+          <h4>
+            <b><i class="fa fa-sort"></i> Sort</b>
+          </h4>
+        </li>
+        <li>
+          <h4>
+            <b><i class="fa fa-filter"></i> Filter</b>
+          </h4>
+        </li>
+      </ul>
     </div>
+  </div>
 </template>
 
 <script>
+import {
+    LOW_TO_HIGH
+  } from '../../store/mutation-types'
 export default {
-    methods: {
-        sortItem () {
-            this.$store.commit(LOW_TO_HIGH, this.sortingValue)
-        },
-        filterItem () {
-            this.$store.commit(LOW_TO_HIGH, this.sortingValue)
-        }
+  methods: {
+    sortItem () {
+      this.$store.commit(LOW_TO_HIGH, this.sortingValue)
+    },
+    filterItem () {
+      this.$store.commit(LOW_TO_HIGH, this.sortingValue)
     }
+  }
 }
 </script>
 <style>
 .mobile-filter {
-    float:left;width:100%;
+  float: left;
+  width: 100%;
 }
 .brd-bottom {
-    border-bottom: 1px solid lightgray;
-    border-top: 1px solid lightgray;
-    margin:5% 0;
+  border-bottom: 1px solid lightgray;
+  border-top: 1px solid lightgray;
+  margin: 5% 0;
 }
 ul.list-inline.mobile-filter li {
-    width: 49%;
-    text-align: center;
+  width: 49%;
+  text-align: center;
 }
 .mobile-filter li:nth-child(1) {
-    border-right:1px solid lightgray;
+  border-right: 1px solid lightgray;
 }
 </style>
-
